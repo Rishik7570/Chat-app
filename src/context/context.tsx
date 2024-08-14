957d8915fb323233ab9f7afc80c738201034e5b8
Rishik7570 <rishiksingha@gmail.com>
2024-08-14 22:21:27 +0530

    Context error

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,6 +1,19 @@
 import { doc, getDoc } from "firebase/firestore";
 import { createContext, ReactNode, useState } from "react";
 import { db } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
+
+
+type userdatatype = {
+    avatar:string,
+    bio:string,
+    email:string,
+    id:string,
+    lastseen:number,
+    name:string,
+    username:string
+}
+
 
 type contextproviderprops = {
     children:ReactNode
@@ -10,20 +23,32 @@ type appcontext = {
 }
 
 
+
 export const Context = createContext<appcontext | null>(null)
 
 const ContextProvider = (props:contextproviderprops)=>{
 
-    const [userdata,setUserdata] = useState()
+    const navigate = useNavigate()
+
+    const [userdata,setUserdata] = useState<userdatatype>()
     const [chatdata,setChatdata] = useState()
+
 
     const loadUserdata = async(uid:string)=>{
         try {
             const userRef = doc(db,'users',uid)
             const userSnap = await getDoc(userRef)
-            console.log(userSnap);
-            
+
+            if(userSnap.exists()){
+                const data = userSnap.data() as userdatatype
+                console.log(data);
+                
+                setUserdata(data)
+            }  
+            console.log(userdata);    
+               
         } catch (error) {
+            console.log(error);
             
         }
     }
